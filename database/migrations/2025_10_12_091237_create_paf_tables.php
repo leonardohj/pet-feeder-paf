d2e23ef0d113be4af96f029f6d8fa0c088c61e7b
Leonardo <leonardo.cheira.ext@proside.pt>
2026-04-02 23:08:30 +0100

    Add migration for feeders, schedules, and feeding logs tables

---
+++ database/migrations/2025_10_12_091237_create_paf_tables.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('feeders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_user')->nullable();
+            $table->string('name')->nullable();
+            $table->boolean('status')->default(false);
+            $table->string('code');
+            $table->string('pet_type')->nullable();
+            $table->date('last_fed_at')->nullable();
+            $table->string('device_token')->unique();
+            $table->timestamps(); 
+        });
+
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('feeder_id')->constrained('feeders')->cascadeOnDelete(); // better than id_feeder
+            $table->time('time');
+            $table->integer('quantity');
+            $table->string('type');
+            $table->json('days')->nullable();
+            $table->timestamps();
+        });
+
+        Schema::create('feeding_logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('feeder_id')->constrained('feeders')->cascadeOnDelete();
+            $table->integer('quantity');
+            $table->integer('status');
+            $table->string('notes')->nullable();
+            $table->date('date');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('feeding_logs');
+        Schema::dropIfExists('schedules');
+        Schema::dropIfExists('feeders');
+    }
+};
